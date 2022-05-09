471eba648e8e214260887a67b0a873e598c7a7af
Aleksei Novikov <alex.nov@list.ru>
2022-05-09 09:37:40 +0300

    Task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая на вход
+принимает число (N), а на выходе показывает все чётные
+числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+ Console.Write("Write number : ");
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        if (i % 2 == 0) 
+            {
+                Console.Write("{0} ", i) ;
+             }
+        
+            
