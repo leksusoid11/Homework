15115ebfd65e01620aa17e3d1b75d0ea9f999fad
Aleksei Novikov <alex.nov@list.ru>
2022-05-17 20:23:05 +0300

    Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 1: Напишите программу, которая принимает на
+вход трёхзначное число и на выходе показывает вторую
+цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int rundomNumber = new Random().Next(100,1000);
+
+Console.Write (rundomNumber);
+
+int number = (rundomNumber % 100) / 10;
+
+Console.Write ($" -> { number} ");
