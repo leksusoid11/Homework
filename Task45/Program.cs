6070b9f7268e8d6cd49ae2717e4e171fed0d8ef3
Aleksei Novikov <alex.nov@list.ru>
2022-06-07 08:40:24 +0300

    Task45

---
+++ Task45/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+*/ 
+
+int[] array = new int[10];
+int arrayCopy;
+
+Console.Write("Оригинал ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 50);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+array[2] = 17; // для проверки
+Console.Write("   Копия ");
+for (int i = 0; i < array.Length; i++)
+{
+    arrayCopy = array[i];
+    Console.Write(arrayCopy + " ");
+} 
