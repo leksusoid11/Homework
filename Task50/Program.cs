079d1ee662e9bd1d2e1a292e79d429d18ff3a897
Aleksei Novikov <alex.nov@list.ru>
2022-06-13 06:52:58 +0300

    Task50

---
+++ Task50/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+Задача 50: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+ и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+17 -> такого числа в массиве нет
+5 -> 9
+*/ 
+
+void FillArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            array[m, n] = new Random().Next(1, 10);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            Console.Write($"{array[m, n]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int m = 3;
+int n = 4;
+int[,] array = new int[m, n];
+
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+string Position(int[,] array)
+{
+    string result = string.Empty;
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            if (num == array[m, n]) result += $"({m}, {n})";
+        }
+    }
+    if (result == string.Empty) result = "Такого элемента нет";
+    return result;
+}
+Console.WriteLine(Position(array));
